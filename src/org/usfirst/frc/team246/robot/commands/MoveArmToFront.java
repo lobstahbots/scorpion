b5b13e6663c9a6bbb089f79a4eccfe088eaf7205
Paul Terrasi <Paul.A.Terrasi@gmail.com>
2015-02-17 21:49:32 -0500

    added potential commands for transitioning to our front

---
+++ src/org/usfirst/frc/team246/robot/commands/MoveArmToFront.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team246.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class MoveArmToFront extends CommandGroup {
+    
+    public  MoveArmToFront() {
+        
+    }
+}
