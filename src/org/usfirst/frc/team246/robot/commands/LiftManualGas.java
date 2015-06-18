dd594f28e96d5d24fe84020a0a098c41eaba2c48
Paul Terrasi <Paul.A.Terrasi@gmail.com>
2015-06-18 15:50:11 -0400

    changes in preperation for Beantown Blitz: speed up fast mode, add gas mode for lift(enabled)

---
+++ src/org/usfirst/frc/team246/robot/commands/LiftManualGas.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team246.robot.commands;
+
+import org.usfirst.frc.team246.robot.Robot;
+import org.usfirst.frc.team246.robot.RobotMap;
+import org.usfirst.frc.team246.robot.subsystems.Forklift;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class LiftManualGas extends Command {
+
+    public LiftManualGas() {
+        requires(Robot.forklift);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	setInterruptible(false);
+    	Robot.forklift.disable();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	RobotMap.liftMotor.set(Robot.oi.operator.getRightTriggerAxis() - Robot.oi.operator.getLeftTriggerAxis());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
